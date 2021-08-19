5b00a6375153a95f3499da6cfcce2d8c02b8ca36
yuchida522 <yuchida522@gmail.com>
2021-08-18 23:06:56 -0700

    add another way to solve

--- intersection.py
+++ intersection.py
@@ -27,6 +27,30 @@ ex. intersection([1, 2, 3, 4], [2, 4, 6, 8], [3, 4, 5])
 
 ###three pointers###
 # have three separate pointers starting at beg of each list
-# 
+#while all of the pointers are not at the end:
+    # if all three pointers have the same number, store it
+    # if not:
+        #find the pointer that has the smallest num
+        #move that pointer along by one
+    
+def intersection(lst1, lst2, lst3):
+    i, j, k = 0, 0, 0
+    intersection = []
 
-print(intersection([1, 2, 5, 4], [2, 4, 5, 8], [3, 0, 5]))
+    while i <len(lst1) and j < len(lst2) and k < len(lst3):
+        if lst1[i] == lst2[j] == lst3[k]:
+            intersection.append(lst1[i])
+            i += 1
+            j += 1
+            k += 1
+        else:
+            smallest = min(lst1[i], lst2[j], lst3[k])
+            if smallest == lst1[i]:
+                i+= 1
+            elif smallest == lst2[j]:
+                j+=1
+            else:
+                k+=1
+    return intersection
+
+print(intersection([1, 2, 3, 4, 5], [2, 4, 5, 6, 8], [3, 4, 5]))
